3e5143195f56d56f3d113bd5070aa034acefbd4a
Bartosz Sławianowski <eplightning@outlook.com>
2020-11-10 10:59:46 +0100

    Change dependencies, modify main

--- cmd/vault-plugin-secrets-minio/main.go
+++ cmd/vault-plugin-secrets-minio/main.go
@@ -10,15 +10,6 @@ import (
 )
 
 func main() {
-	logger := hclog.New(&hclog.LoggerOptions{})
-
-	defer func() {
-		if r := recover(); r != nil {
-			logger.Error("panic in the plugin", "error", r)
-			os.Exit(1)
-		}
-	}()
-
 	meta := &api.PluginAPIClientMeta{}
 
 	flags := meta.FlagSet()
@@ -31,6 +22,8 @@ func main() {
 		BackendFactoryFunc: minio.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
 	}); err != nil {
+		logger := hclog.New(&hclog.LoggerOptions{})
+
 		logger.Error("plugin shutting down", "error", err)
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/hashicorp/errwrap v1.1.0
 	github.com/hashicorp/go-hclog v0.15.0
-	github.com/hashicorp/vault/api v1.0.4
-	github.com/hashicorp/vault/sdk v0.1.13
+	github.com/hashicorp/vault/api v1.0.5-0.20200215224050-f6547fa8e820
+	github.com/hashicorp/vault/sdk v0.1.14-0.20200215224050-f6547fa8e820
 	github.com/minio/minio v0.0.0-20201109180501-8e8ddf7233ed
 )
